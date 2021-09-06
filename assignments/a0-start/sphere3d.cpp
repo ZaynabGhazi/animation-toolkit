665500eb7c45a6bafbd558bdfc3701d5d789fea6
Zaynab <ghazii.zaynab@gmail.com>
2021-09-05 21:35:24 -0400

    animate 3d sphere with random movement

--- assignments/a0-start/sphere3d.cpp
+++ assignments/a0-start/sphere3d.cpp
@@ -1,23 +1,49 @@
 #include "atkui/framework.h"
+#include <unistd.h>
 
 using glm::vec3;
 
-class Sphere3D : public atkui::Framework {
- public:
-  Sphere3D() : atkui::Framework(atkui::Perspective) {
+class Sphere3D : public atkui::Framework
+{
+public:
+  Sphere3D() : atkui::Framework(atkui::Perspective)
+  {
   }
 
-  virtual void scene() {
-    // colors are RGB triplets in range [0,1]
-    setColor(vec3(0,1,0));
+private:
+  vec3 position = vec3(0, 0, 0);
+  vec3 direction;
+  float radius = 50.0;
+  float velocity = 50;
+  bool isSliding = false;
 
-    // draw a sphere at center of the world
-    float radius = 50.0;
-    drawSphere(vec3(0), radius);
+  virtual void scene()
+  {
+    setColor(vec3(0, 1, 0));
+    if (isSliding)
+    {
+      position += direction * dt() * velocity;
+    }
+    drawSphere(position, radius);
+  }
+
+  virtual void keyUp(int key, int mods)
+  {
+    if (key == GLFW_KEY_SPACE)
+    {
+      isSliding = true;
+      direction = agl::randomUnitVector();
+    }
+    if (key == GLFW_KEY_R)
+    {
+      //reset to origin
+      isSliding = false;
+      position = vec3(0, 0, 0);
+    }
   }
 };
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   Sphere3D viewer;
   viewer.run();
