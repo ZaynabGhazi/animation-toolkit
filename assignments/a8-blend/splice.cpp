928cb5131f1043d0cb3f3d03e73a47ca56d337b4
Zaynab <ghazii.zaynab@gmail.com>
2021-11-12 14:38:37 -0500

    splice

--- assignments/a8-blend/splice.cpp
+++ assignments/a8-blend/splice.cpp
@@ -24,33 +24,57 @@ public:
       _splice = spliceUpperBody(_lower, _upper, _alpha);
    }
 
-   Motion spliceUpperBody(const Motion& lower, const Motion& upper, float alpha)
+   Motion spliceUpperBody(const Motion &lower, const Motion &upper, float alpha)
    {
       Motion result;
       result.setFramerate(lower.getFramerate());
-      // todo: your code here
-      result.appendKey(lower.getKey(0));
+      for (int i = 0; i < lower.getNumKeys(); i++)
+      {
+         Pose current = lower.getKey(i);
+         Pose splice = upper.getKey(i+120);
+         Pose final = current;
+         Joint *spine = _skeleton.getByName("Beta:Spine1");
+         if (spine == NULL) std::cout<<"null spine"<<std::endl;
+         for (int i = 0; i < current.jointRots.size(); i++)
+         {
+            bool upper_ = false;
+            Joint *current_joint = _skeleton.getByID(i);
+            Joint *parent = current_joint->getParent();
+            while (parent != NULL && parent != _skeleton.getRoot())
+            {
+               if (parent->getID() == spine->getID())
+               {
+                  upper_ = true;
+                  break;
+               }
+               parent = parent->getParent();
+            }
+            if (upper_)
+               final.jointRots.at(i) = slerp(current.jointRots.at(i), splice.jointRots.at(i), alpha);
+         }
+         result.appendKey(final);
+      }
       return result;
    }
 
    void scene()
-   {  
+   {
       _splice.update(_skeleton, elapsedTime() * 0.5);
       SkeletonDrawer drawer;
       drawer.draw(_skeleton, *this);
-      drawText("alpha: "+std::to_string(_alpha), 10, 15);
+      drawText("alpha: " + std::to_string(_alpha), 10, 15);
    }
 
-   void keyUp(int key, int mods) 
+   void keyUp(int key, int mods)
    {
       if (key == GLFW_KEY_UP)
       {
-         _alpha = std::min(_alpha+0.05, 1.0);
+         _alpha = std::min(_alpha + 0.05, 1.0);
          _splice = spliceUpperBody(_lower, _upper, _alpha);
       }
       else if (key == GLFW_KEY_DOWN)
       {
-         _alpha = std::max(_alpha-0.05, 0.0);
+         _alpha = std::max(_alpha - 0.05, 0.0);
          _splice = spliceUpperBody(_lower, _upper, _alpha);
       }
    }
@@ -62,10 +86,9 @@ public:
    float _alpha;
 };
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
    ASplice viewer;
    viewer.run();
    return 0;
 }
-
