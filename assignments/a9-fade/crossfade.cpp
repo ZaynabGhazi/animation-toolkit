9de4fe2f92f197ea6b216fafa98fd987f54d1bd3
Zaynab <ghazii.zaynab@gmail.com>
2021-11-21 23:10:54 -0500

    finish crossfade

--- assignments/a9-fade/crossfade.cpp
+++ assignments/a9-fade/crossfade.cpp
@@ -38,30 +38,41 @@ public:
     int start2 = 0;
 
     //fix speed
-    blend_.setFramerate(motion1_.getFramerate());    
-     for(int i=0; i < start1;i++){
+    blend_.setFramerate(motion1_.getFramerate());
+    for (int i = 0; i < start1; i++)
+    {
       blend_.appendKey(motion1_.getKey(i));
-    }  
-
+    }
+    Pose initial = motion1_.getKey(motion1_.getNumKeys() - 1);
+    align(initial, motion2_);
     //Blend
-    for(int i=0; i < numBlendFrames; i++){
-      Pose pose_1 = motion1_.getKey(start1+i);
-      Pose pose_2 = motion2_.getKey(start2+i);
-      Pose blended = Pose::Lerp(pose_1,pose_2,(float)i/(numBlendFrames-1));
+    for (int i = 0; i < numBlendFrames; i++)
+    {
+      Pose pose_1 = motion1_.getKey(start1 + i);
+      Pose pose_2 = motion2_.getKey(start2 + i);
+      Pose blended = Pose::Lerp(pose_1, pose_2, (float)i / (numBlendFrames - 1));
       blend_.appendKey(blended);
     }
-    for(int i=numBlendFrames; i < motion2_.getNumKeys();i++){
+    for (int i = numBlendFrames; i < motion2_.getNumKeys(); i++)
+    {
       blend_.appendKey(motion2_.getKey(i));
     }
-    
   }
+  void align(Pose &initial, Motion &motion)
+  {
+    float heading = glm::eulerAngles(initial.jointRots[0]).y;
+    Pose first = motion.getKey(0);
+    Transform shift(angleAxis(0.0f, vec3(1, 0, 0)), -first.rootPos);
+    Transform rotate(angleAxis(heading, vec3(0, 1, 0)), initial.rootPos);
 
-  void append_motions(){
-    for(int i=0; i < motion1_.getNumKeys();i++){
-      blend_.appendKey(motion1_.getKey(i));
-    }  
-    for(int i=0; i < motion2_.getNumKeys();i++){
-      blend_.appendKey(motion2_.getKey(i));
+    for (int i = 0; i < motion.getNumKeys(); i++)
+    {
+      Pose pose = motion.getKey(i);
+      Transform current(pose.jointRots[0], pose.rootPos);
+      Transform final = rotate * shift * current;
+      pose.jointRots[0] = final.r();
+      pose.rootPos = final.t();
+      motion.editKey(i, pose);
     }
   }
 
