75f9f5bad3129d523704441aee556dc0f2754057
Zaynab <ghazii.zaynab@gmail.com>
2021-12-01 00:37:15 -0500

    implement look

--- assignments/a10-ik/looker.cpp
+++ assignments/a10-ik/looker.cpp
@@ -8,32 +8,45 @@ using namespace atk;
 using namespace glm;
 using namespace std;
 
-class ALooker : public atkui::Framework {
+class ALooker : public atkui::Framework
+{
 public:
    ALooker() : atkui::Framework(atkui::Perspective) {}
    virtual ~ALooker() {}
 
-   void setup() {
+   void setup()
+   {
       Motion motion;
       BVHReader reader;
       reader.load("../motions/Beta/idle.bvh", _skeleton, motion);
       motion.update(_skeleton, 0);
 
-      _drawer.color = vec3(1,0,0);
+      _drawer.color = vec3(1, 0, 0);
       _head = _skeleton.getByName("Beta:Head");
    }
 
-   void reset(Joint* head) {
+   void reset(Joint *head)
+   {
       head->setLocalRotation(IdentityQ);
       head->fk();
    }
 
-   void lookAtTarget(Joint* head, const vec3& target) {
-      // TODO: Your code here
+   void lookAtTarget(Joint *head, const vec3 &target)
+   {
+      // Follow lecture 19
+      vec3 desired_direction = target - head->getGlobalTranslation();
+      vec3 Z = normalize(desired_direction);
+      vec3 X = cross(vec3(0, 1, 0),Z);
+      vec3 Y = cross(Z, X);
+      mat3 rot = mat3(normalize(X), normalize(Y), normalize(Z));
+      // convert to local
+      quat local_rot = head->getLocal2Global().inverse().r() * quat(rot);
+      head->setLocalRotation(local_rot * head->getLocalRotation());
       head->fk();
    }
 
-   void scene() {  
+   void scene()
+   {
       float r = 100;
       float angle = elapsedTime();
       _target = vec3(r * cos(angle), r * sin(angle) + r, 150);
@@ -43,22 +56,22 @@ public:
       _drawer.draw(_skeleton, *this);
 
       vec3 globalHeadPos = _head->getGlobalTranslation();
-      vec3 globalForward = _head->getGlobalRotation() * vec3(0,0,1);
+      vec3 globalForward = _head->getGlobalRotation() * vec3(0, 0, 1);
 
-      setColor(vec3(0,0,1));
-      drawLine(globalHeadPos, globalHeadPos + 200.0f*globalForward);
+      setColor(vec3(0, 0, 1));
+      drawLine(globalHeadPos, globalHeadPos + 200.0f * globalForward);
       drawSphere(_target, 5);
    }
 
    Cyclops _drawer;
    Skeleton _skeleton;
-   Joint* _head;
+   Joint *_head;
    vec3 _target;
 };
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
    ALooker viewer;
    viewer.run();
    return 0;
 }
-
