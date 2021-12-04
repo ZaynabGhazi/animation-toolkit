566f69c8e70a49885a86a12ecba1f00f94d045b4
Zaynab <ghazii.zaynab@gmail.com>
2021-12-03 21:01:21 -0500

    implement analytic approach

--- assignments/a10-ik/ik_controller.cpp
+++ assignments/a10-ik/ik_controller.cpp
@@ -12,26 +12,55 @@ bool IKController::solveIKAnalytic(Skeleton &skeleton,
   if (jointid == -1)
     return true;
 
-  Joint *ankle = skeleton.getByID(jointid);
-  if (!ankle->getParent())
+  Joint *j3 = skeleton.getByID(jointid);
+  if (!j3->getParent())
   {
     std::cout << "Warning: solveIKAnalytic() needs joint "
                  "with parent and grandparent\n";
     return false;
   }
 
-  Joint *knee = ankle->getParent();
-  if (!knee->getParent())
+  Joint *j2 = j3->getParent();
+  if (!j2->getParent())
   {
     std::cout << "Warning: solveIKAnalytic() needs joint "
                  "with parent and grandparent\n";
     return false;
   }
-
-  Joint *hip = knee->getParent();
-
-  // TODO: Your code here
-  return true;
+  Joint *j1 = j2->getParent();
+  // from lecture notes
+  // PART1
+  float l1 = length(j2->getGlobalTranslation() - j1->getGlobalTranslation());
+  float l2 = length(j3->getGlobalTranslation() - j2->getGlobalTranslation());
+  float r = length(goalPos - j1->getGlobalTranslation());
+  float cos_phi = (r * r - l1 * l1 - l2 * l2) / (-2.0f * l1 * l2);
+  cos_phi > 1 ? cos_phi = 1 : cos_phi = (cos_phi < -1 ? -1 : cos_phi);
+  float phi = acos(cos_phi);
+  float theta_2z = phi - M_PI;
+  vec3 limbDir = normalize(j2->getLocalTranslation());
+  vec3 axis = cross(limbDir, vec3(0, 0, -1));
+  if (limbDir[1] < 0)
+    axis = cross(limbDir, vec3(0, 0, 1));
+  quat R21 = angleAxis(theta_2z, axis);
+  Transform T21(R21, j2->getLocalTranslation());
+  j2->setLocal2Parent(T21);
+  skeleton.fk();
+  // PART2: Method 2
+  vec3 p3 = j3->getGlobalTranslation();
+  vec3 r_ = p3 - j1->getGlobalTranslation();
+  vec3 e_ = goalPos - p3;
+  vec3 r_x_e = cross(r_, e_);
+  float phi_ = atan2(length(r_x_e), dot(r_, r_) + dot(r_, e_));
+  vec3 axis_ = normalize(r_x_e);
+  if (length(r_x_e) == 0)
+    axis_ = vec3(0, 0, 1);
+  axis_ = j1->getLocal2Global().inverse().transformVector(axis_);
+  Transform T10 = Transform(angleAxis(phi_, axis_), vec3(0));
+  j1->setLocal2Parent(j1->getLocal2Parent() * T10);
+  skeleton.fk();
+  if (length(goalPos - j3->getGlobalTranslation()) <= epsilon)
+    return true;
+  return false;
 }
 
 bool IKController::solveIKCCD(Skeleton &skeleton, int jointid,
@@ -59,7 +88,8 @@ bool IKController::solveIKCCD(Skeleton &skeleton, int jointid,
       vec3 r_x_e = cross(r_, e_);
       float delta_phi = nudgeFactor * atan2(length(r_x_e), dot(r_, r_) + dot(r_, e_));
       vec3 axis = normalize(r_x_e);
-      if (length(r_x_e)==0) axis = vec3(0,0,1);
+      if (length(r_x_e) == 0)
+        axis = vec3(0, 0, 1);
       axis = current->getLocal2Global().inverse().transformVector(axis);
       Transform rotate(angleAxis(delta_phi, axis), vec3(0));
       current->setLocal2Parent(current->getLocal2Parent() * rotate);
